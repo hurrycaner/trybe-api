50905f7dec2a150a4d42fe746c849cb22e4a41dc
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-21 16:17:46 -0300

    Add inital tests for show action

--- spec/requests/admin/v1/users_request_spec.rb
+++ spec/requests/admin/v1/users_request_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe 'Admin::V1::Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context 'GET /users/:id' do
+    let(:user) { create(:user) }
+    let(:url) { "/admin/v1/users#{user.id}" }
+    before(:each) { get url, headers: auth_header(user) }
+
+    it 'returns the requested User' do
+      expected_user = user.as_json(only: %i[id name image email profile])
+      expect(json_body['user']).to eq expected_user
+    end
+
+    it 'returns success status' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
