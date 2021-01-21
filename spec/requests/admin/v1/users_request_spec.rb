15dce5d77d34b782cd29bbc61133dfa6e1b05dbf
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-21 15:57:57 -0300

    ADD users index action and view

--- spec/requests/admin/v1/users_request_spec.rb
+++ spec/requests/admin/v1/users_request_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe 'Admin::V1::Users', type: :request do
 
   context 'GET /users' do
     let(:url) { '/admin/v1/users' }
-    let!(:users) { create_list(:user, 10) }
+    let!(:users) { create_list(:user, 3) }
     before(:each) { get url, headers: auth_header(user) }
 
     it 'returns all Users' do
-      expect(json_body['users']).to contain_exactly(*users.as_json)
+      # add the current user to the users list to make sure all users are returned
+      users << user
+      expect(json_body['users']).to contain_exactly(*users.as_json(only: %i[id name image email profile]))
     end
 
     it 'returns success status' do
