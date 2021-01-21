17144bcffd44b765ddec5c96faa0b262b04d7931
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-20 21:56:20 -0300

    Implement the validations on user resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
+
+  validates :name, :profile, :image, presence: true
+
+  enum profile: { admin: 0, client: 1 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-  it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_presence_of(:image) }
   it { is_expected.to validate_presence_of(:profile) }
 
