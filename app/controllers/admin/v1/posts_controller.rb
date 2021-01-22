f3ecbf328ecfac2c8bdc618c8093ac74e6d2338b
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-22 16:02:02 -0300

    Add Post List

--- app/controllers/admin/v1/posts_controller.rb
+++ app/controllers/admin/v1/posts_controller.rb
@@ -1,2 +1,9 @@
-class Admin::V1::PostsController < ApplicationController
+module Admin::V1
+  class PostsController < ApiController
+    skip_before_action :authenticate_user!, only: :index
+
+    def index
+      @posts = Post.all
+    end
+  end
 end
