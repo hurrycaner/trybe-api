bb7bec92852ff06e8ded87bd5d5e584a2c6efc2a
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-25 13:36:48 -0300

    Change the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,5 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to have_many(:posts).dependent(:destroy) }
 
-  it_behaves_like 'name searchable concern', :user
+  it_behaves_like 'like searchable concern', :user, :name
 end
