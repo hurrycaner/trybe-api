1796696e3b9d1a5bae13558206cfedd072797e12
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-01-26 23:31:48 -0300

    Chore: Update the landing page link

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 class LandingController < ApplicationController
   def landing
-    render html: 'Bem vindo a TRYBE-API! Acesse <a href="https://trybe-api.herokuapp.com">aqui</a> para visualizar a documentação'.html_safe
+    render html: 'Bem vindo a TRYBE-API! Acesse <a href="https://github.com/jramiresbrito/trybe-api">aqui</a> para visualizar a documentação'.html_safe
   end
 end
